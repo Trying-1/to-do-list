e9cbb0e450f88ecd561ba3d6210c963d7648dd53
Trying-100 <tryingbait@gmail.com>
2024-04-25 22:39:20 +0000

    basic add and delete logic

--- to-do-list/src/components/TodoList.jsx
+++ to-do-list/src/components/TodoList.jsx
@@ -2,27 +2,34 @@ import React, { useState } from 'react'
 import ListItem from './ListItem'
 
 function TodoList() {
-    const [task, setnewtask] = useState("");
-    const [tasks, setTasks] = useState(["write", "trade", "run", "code", "eat"])
+    const [newtask, setnewtask] = useState("");//this for the input field
+    const [tasks, setTasks] = useState(['hello', 'joy']);//this array store the list of tasks
+
     const handleoninputchange = (e) => {
         setnewtask(e.target.value);
     }
 
-    const handleaddtask = () => {
-        setTasks(...tasks, task)
-        setnewtask("");
 
+    const handleaddtask = () => {
+        if (newtask.trim() !== "") { //check if the input is empty
+            setTasks([...tasks, newtask]);
+            setnewtask(""); // Clearing the input field
+        }
+        else {
+            console.log("enter the task")
+        }
     }
 
     return (
         <div className='todolist'>
             <div className="input-area">
-                <input className='input-field' placeholder='Add task' onChange={handleoninputchange} />
+                <input className='input-field' placeholder='Add task' onChange={handleoninputchange} value={newtask}  // Binding value to state
+                />
                 <button className='add-btn btn' onClick={handleaddtask}>Add</button>
             </div>
             <div className="list-area">
                 <div className="listitem">
-                    <ListItem tasks={tasks} />
+                    <ListItem tasks={tasks} setTasks={setTasks} />
                 </div>
             </div>
         </div>
